021ed11494894fa68dc6eb6f1732db1fc560cecb
FruitsBerriesMelons123 <60863689+FruitsBerriesMelons123@users.noreply.github.com>
2022-12-11 19:15:37 -0700

    update to 0.0.10

--- skse64/alchemist/src/main.cpp
+++ skse64/alchemist/src/main.cpp
@@ -3,13 +3,11 @@
 
 #include "main.h"
 
-#include <thread>
 #include <mutex>
 #include <vector>
 
 using std::vector;
 using std::mutex;
-using std::thread;
 
 namespace alchemist {
 	Potion costliestPotion;
@@ -157,11 +155,12 @@ namespace alchemist {
 	void makePotions() {
 		potions.clear();
 
-		int max_threads = thread::hardware_concurrency();
-		int num_threads = 2;
-		if (max_threads > 2) {
-			num_threads = max_threads - 1;
-		}
+		//int max_threads = thread::hardware_concurrency();
+		//int num_threads = 2;
+		//if (max_threads > 2) {
+		//	num_threads = max_threads - 1;
+		//}
+		int num_threads = kActualThreadsToUseInGame.GetData().i;
 		alchemist_mutex.lock();
 		ingredients_it1 = ingredients.begin();
 		ingredients_it2 = ingredients_it1;
@@ -222,8 +221,137 @@ namespace alchemist {
 		}
 	}
 
+	void improvePotionST(Potion potion) {
+		for (auto ingredientIt = ingredients.begin(); ingredientIt != ingredients.end(); ++ingredientIt) {
+			if (potion.ingredient1 == *ingredientIt || potion.ingredient2 == *ingredientIt) {
+				continue;
+			}
+			set<Effect> effects = potion.effects;
+			Effect controlEffect = potion.controlEffect;
+			float controlCost = potion.controlEffect.calcCost;
+			for (Effect effect : potion.possibleEffects) {
+				auto effectIt = ingredientIt->effects.find(effect);
+				if (effectIt != ingredientIt->effects.end()) {
+					float costCheck = 0;
+					Effect effectCheck;
+					if (effectIt->calcCost > effect.calcCost) {
+						costCheck = effectIt->calcCost;
+						effectCheck = *effectIt;
+						effects.insert(*effectIt);
+					}
+					else {
+						costCheck = effect.calcCost;
+						effectCheck = effect;
+						effects.insert(effect);
+					}
+					if (costCheck > controlCost) {
+						controlEffect = effectCheck;
+						controlCost = costCheck;
+					}
+				}
+			}
+			if (effects.size() == potion.effects.size()) {
+				continue;
+			}
+			float cost = 0;
+			for (Effect effect : effects) {
+				if (!(player.hasPerkPurity && effect.beneficial && !controlEffect.beneficial) &&
+					!(player.hasPerkPurity && !effect.beneficial && controlEffect.beneficial)) {
+					cost += effect::getPerkCalcCost(effect, controlEffect.beneficial);
+				}
+			}
+			Potion improvedPotion = Potion(3, potion.ingredient1, potion.ingredient2, *ingredientIt, effects, controlEffect, cost);
+			potions.insert(improvedPotion);
+			if (floor(cost) > costliestPotion.cost) {
+				costliestPotion = improvedPotion;
+			}
+		}
+	}
+
+	void makePotionsST2(Ingredient ingredient1, Ingredient ingredient2) {
+		if (ingredient1 == ingredient2) {
+			return;
+		}
+		set<Effect> effects;
+		set<Effect> possibleEffects = ingredient1.effects;
+		possibleEffects.merge(ingredient2.effects);
+		Effect controlEffect;
+		float controlCost = 0;
+		for (auto it1 = ingredient1.effects.begin(); it1 != ingredient1.effects.end(); ++it1) {
+			auto it2 = ingredient2.effects.find(*it1);
+			if (it2 != ingredient2.effects.end()) {
+				possibleEffects.erase(*it1);
+				float costCheck = 0;
+				Effect effectCheck;
+				if (it1->calcCost > it2->calcCost) {
+					costCheck = it1->calcCost;
+					effectCheck = *it1;
+					effects.insert(*it1);
+				}
+				else {
+					costCheck = it2->calcCost;
+					effectCheck = *it2;
+					effects.insert(*it2);
+				}
+				if (costCheck > controlCost) {
+					controlEffect = effectCheck;
+					controlCost = costCheck;
+				}
+			}
+		}
+		if (controlCost == 0) {
+			return;
+		}
+		float cost = 0;
+		for (Effect effect : effects) {
+			if (!(player.hasPerkPurity && effect.beneficial && !controlEffect.beneficial) &&
+				!(player.hasPerkPurity && !effect.beneficial && controlEffect.beneficial)) {
+				cost += effect::getPerkCalcCost(effect, controlEffect.beneficial);
+			}
+		}
+		Potion potion = Potion(2, ingredient1, ingredient2, effects, possibleEffects, controlEffect, cost);
+		potions.insert(potion);
+		if (floor(cost) > costliestPotion.cost) {
+			costliestPotion = potion;
+		}
+	}
+
+	void makePotionsST() {
+		potions.clear();
+		auto it_end = ingredients.end();
+		for (auto it1 = ingredients.begin(); it1 != it_end;) {
+			Ingredient ingredient1 = *it1;
+			for (auto it2 = ++it1; it2 != it_end; ++it2) {
+				Ingredient ingredient2 = *it2;
+				makePotionsST2(ingredient1, ingredient2);
+			}
+		}
+		for (Potion potion : potions) {
+			improvePotionST(potion);
+		}
+
+		string effectDescriptions = "";
+		for (auto effect : costliestPotion.effects) {
+			if (!(player.hasPerkPurity && effect.beneficial && !costliestPotion.controlEffect.beneficial) &&
+				!(player.hasPerkPurity && !effect.beneficial && costliestPotion.controlEffect.beneficial)) {
+				effectDescriptions += " " + effect::getPerkCalcDescription(effect, costliestPotion.controlEffect.beneficial);
+			}
+		}
+		if (costliestPotion.size == 2) {
+			costliestPotion.description = costliestPotion.name + ":" + effectDescriptions +
+				"\n Value: " + str::fromFloat(floor(costliestPotion.cost)) + "\n" + str::printSort2(costliestPotion.ingredient1.name, costliestPotion.ingredient2.name);
+		}
+		else if (costliestPotion.size == 3) {
+			costliestPotion.description = costliestPotion.name + ":" + effectDescriptions +
+				"\n Value: " + str::fromFloat(floor(costliestPotion.cost)) + "\n" + str::printSort3(costliestPotion.ingredient1.name, costliestPotion.ingredient2.name, costliestPotion.ingredient3.name);
+		}
+	}
+
 	void initAlchemist() {
-		player.init();
+		int ignorePlayer = kIgnorePlayer.GetData().i;
+		if (ignorePlayer == 0) {
+			player.init();
+		}
 		ExtraContainerChanges* containerChanges = static_cast<ExtraContainerChanges*>((*g_thePlayer)->extraData.GetByType(kExtraData_ContainerChanges));
 		ExtraContainerChanges::Data* containerData = containerChanges ? containerChanges->data : NULL;
 		EntryDataList* objList = containerData->objList;
@@ -237,72 +365,61 @@ namespace alchemist {
 		}
 		VMResultArray<TESForm*> playerForms = papyrusObjectReference::GetContainerForms(*g_thePlayer);
 		ingredients.clear();
-		for (TESForm* form : playerForms) {
-			if (form->GetFormType() == kFormType_Weapon) {
-				TESObjectWEAP* item = DYNAMIC_CAST(form, TESForm, TESObjectWEAP);
-				InventoryEntryData::EquipData itemData;
-				containerData->GetEquipItemData(itemData, item, item->formID);
-				if (itemData.isTypeWorn) {
-					if (item->enchantable.enchantment) {
-						for (int i = 0; i < item->enchantable.enchantment->effectItemList.count; ++i) {
-							MagicItem::EffectItem* effect = item->enchantable.enchantment->effectItemList[i];
-							if (effect::getName(effect) == "Fortify Alchemy") {
-								player.fortifyAlchemyLevel += effect::getMagnitude(effect);
+		if (ignorePlayer == 0) {
+			for (TESForm* form : playerForms) {
+				if (form->GetFormType() == kFormType_Weapon) {
+					TESObjectWEAP* item = DYNAMIC_CAST(form, TESForm, TESObjectWEAP);
+					InventoryEntryData::EquipData itemData;
+					containerData->GetEquipItemData(itemData, item, item->formID);
+					if (itemData.isTypeWorn) {
+						if (item->enchantable.enchantment) {
+							for (int i = 0; i < item->enchantable.enchantment->effectItemList.count; ++i) {
+								MagicItem::EffectItem* effect = item->enchantable.enchantment->effectItemList[i];
+								if (effect::getName(effect) == "Fortify Alchemy") {
+									player.fortifyAlchemyLevel += effect::getMagnitude(effect);
+								}
 							}
 						}
 					}
 				}
-			}
-			else if (form->GetFormType() == kFormType_Armor) {
-				TESObjectARMO* item = DYNAMIC_CAST(form, TESForm, TESObjectARMO);
-				InventoryEntryData::EquipData itemData;
-				containerData->GetEquipItemData(itemData, item, item->formID);
-				if (itemData.isTypeWorn) {
-					if (item->enchantable.enchantment) {
-						for (int i = 0; i < item->enchantable.enchantment->effectItemList.count; ++i) {
-							MagicItem::EffectItem* effect = item->enchantable.enchantment->effectItemList[i];
-							if (effect::getName(effect) == "Fortify Alchemy") {
-								player.fortifyAlchemyLevel += effect::getMagnitude(effect);
+				else if (form->GetFormType() == kFormType_Armor) {
+					TESObjectARMO* item = DYNAMIC_CAST(form, TESForm, TESObjectARMO);
+					InventoryEntryData::EquipData itemData;
+					containerData->GetEquipItemData(itemData, item, item->formID);
+					if (itemData.isTypeWorn) {
+						if (item->enchantable.enchantment) {
+							for (int i = 0; i < item->enchantable.enchantment->effectItemList.count; ++i) {
+								MagicItem::EffectItem* effect = item->enchantable.enchantment->effectItemList[i];
+								if (effect::getName(effect) == "Fortify Alchemy") {
+									player.fortifyAlchemyLevel += effect::getMagnitude(effect);
+								}
 							}
 						}
 					}
 				}
 			}
 		}
+		int protectIngredients = kProtectIngredients.GetData().i;
 		for (TESForm* form : playerForms) {
 			if (form->GetFormType() == kFormType_Ingredient) {
 				IngredientItem* ingredient = DYNAMIC_CAST(form, TESForm, IngredientItem);
-				if (!ingredient::isProtected(ingredient, ingredientCount)) {
+				if (protectIngredients == 0 || !ingredient::isProtected(ingredient, ingredientCount)) {
 					ingredients.insert(Ingredient(ingredient));
 				}
 			}
 		}
-		player.setState();
+		if (ignorePlayer == 0) {
+			player.setState();
+		}
 	}
 
-	// DEBUG tests lol
-	// 115 ingredients = 56 seconds
-	// 99 ingredients = 32 seconds
-	// 90 ingredients = 23 seconds
-	// 81 ingredients = 15 seconds
-	// 70 ingredients = 9 seconds
-	// 60 ingredients = 4 seconds
-	// 53 ingredients = 3 seconds
-	// 47 ingredients = 2 seconds
-	// 34 ingredients = 1 second
-	// RELEASE tests
-	// 115 ingredients = 5 seconds
-	// 108 ingredients = 4 seconds
-	// 103 ingredients = 3 seconds
-	// 91 ingredients = 2 seconds
-	// 71 ingredients = 1 second
-	// MULTITHREAD tests
-	// 115 ingredients = 0 seconds
+	// SINGLETHREADED: all 177 ingredients in 19 seconds, and no crashing 
+	// MULTITHREADED: any more than 102 or 103 ingredients and it crashes (your results may differ)
 	void stressTest() {
+		int stressTestCount = kNumberOfIngredientsToStressTest.GetData().i;
 		ingredients.clear();
 		auto allIngredients = DataHandler::GetSingleton()->ingredients;
-		for (int i = 0; i < allIngredients.count; ++i) {
-			//for (int i = 0; i < 71; ++i) {
+		for (int i = 0; i < allIngredients.count && i < stressTestCount; ++i) {
 			ingredients.insert(Ingredient(allIngredients[i]));
 		}
 	}
@@ -310,24 +427,38 @@ namespace alchemist {
 	class Scaleform_RegisterGetBestRecipeNameHandler : public GFxFunctionHandler {
 	public:
 		virtual void Invoke(Args* args) {
+			static int performStressTest = 0;
+			performStressTest = kNumberOfIngredientsToStressTest.GetData().i;
 			static string alchemist_result = "";
 			alchemist_result = "";
 			if (args && args->numArgs && args->numArgs > 0) {
 				string craft_description = *(args->args[0].data.managedString);
 				if (craft_description.find("Alchemy") != string::npos && g_thePlayer) {
-					//time_t start = time(NULL);
+					static time_t start;
+					if (performStressTest > 0) {
+						start = time(NULL);
+					}
 					initAlchemist();
-					//stressTest();
+					if (performStressTest > 0) {
+						stressTest();
+					}
 					if (ingredients != lastIngredientList || player.state != player.lastState) {
 						costliestPotion = Potion(0, "No potion recipes are currently available.");
 						lastIngredientList = set<Ingredient>(ingredients);
 						player.lastState = player.state;
-						makePotions();
+						if (kActualThreadsToUseInGame.GetData().i < 2) {
+							makePotionsST();
+						}
+						else {
+							makePotions();
+						}
 					}
 					alchemist_result = costliestPotion.description;
-					//time_t end = time(NULL);
-					//_LOG(str::fromInt(end - start) + " seconds");
-					//_LOG(str::fromInt(ingredients.size()) + " ingredients");
+					if (performStressTest > 0) {
+						time_t end = time(NULL);
+						_LOG(str::fromInt(end - start) + " seconds");
+						_LOG(str::fromInt(ingredients.size()) + " ingredients");
+					}
 				}
 			}
 			if (args && args->result) {
@@ -380,8 +511,44 @@ extern "C" {
 		return true;
 	}
 
+	void MessageHandler(SKSEMessagingInterface::Message* msg)
+	{
+		switch (msg->type)
+		{
+		case SKSEMessagingInterface::kMessage_InputLoaded:
+		{
+			_MESSAGE("...prosperous alchemist initialized!");
+		}
+		break;
+		}
+	}
+
 	bool SKSEPlugin_Load(const SKSEInterface* a_skse) {
-		_MESSAGE("[MESSAGE] alchemist loaded");
+		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\alchemist.log");
+		_MESSAGE("[MESSAGE] Initializing prosperous alchemist...");
+
+		kPluginHandle = a_skse->GetPluginHandle();
+		kMsgInterface = (SKSEMessagingInterface*)a_skse->QueryInterface(kInterface_Messaging);
+
+		if (!kMsgInterface)
+		{
+			_MESSAGE("Couldn't initialize messaging interface");
+			return false;
+		}
+		else if (kMsgInterface->interfaceVersion < 2)
+		{
+			_MESSAGE("Messaging interface too old (%d expected %d)", kMsgInterface->interfaceVersion, 2);
+			return false;
+		}
+
+		_MESSAGE("Initializing prosperous alchemist INI Manager");
+		AlchemistINIManager::Instance.Initialize("Data\\SKSE\\Plugins\\alchemist.ini", nullptr);
+
+		if (kMsgInterface->RegisterListener(kPluginHandle, "SKSE", MessageHandler) == false)
+		{
+			_MESSAGE("Couldn't register message listener");
+			return false;
+		}
 
 		SKSEScaleformInterface* scaleformInterface = (SKSEScaleformInterface*)a_skse->QueryInterface(kInterface_Scaleform);
 
@@ -389,7 +556,7 @@ extern "C" {
 
 		if (registerScaleformHandlers) {
 			srand(time(NULL));
-			_MESSAGE("[MESSAGE] scaleform handlers registered");
+			_MESSAGE("[MESSAGE] prosperous alchemist scaleform handlers registered");
 		}
 
 		return true;
@@ -402,11 +569,11 @@ extern "C" {
 
 		1,
 		"Prosperous Alchemist",
-
-		"my name",
-		"support@example.com",
+		"FruitsBerriesMelons123",
+		"",
 
 		0,	// not version independent
+
 		{ CURRENT_RELEASE_RUNTIME, 0 },
 
 		0,	// works with any version of the script extender. you probably do not need to put anything here
